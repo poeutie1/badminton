552870b696ce09ee5fcad3087fbd55d2bba811be
佐藤里桜 <satorio@MacBook-Air-de-safujisatosakura.local>
2025-08-08 17:51:13 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // これを指定すると `npm run build` 時に静的エクスポート(outフォルダ)が出力される
-  // output: "export",
-  trailingSlash: true,
-  // 必要であれば、URL末尾にスラッシュを付けたい場合はコメントを外してください
-  // trailingSlash: true,
-};
+const nextConfig = {};
 
 module.exports = nextConfig;

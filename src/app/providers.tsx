8b55be63146c71a2e7b3546045b1d6aa71f6cacb
佐藤里桜 <satorio@MacBook-Air-de-safujisatosakura.local>
2025-08-08 18:38:23 +0900

    test

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,8 +2,7 @@
 "use client";
 
 import { ReactNode } from "react";
-import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }: { children: ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <>{children}</>;
 }
